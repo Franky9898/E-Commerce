b8139837f4483a4b46b0ccccb0d925b3fbde66fd
Francesco <martinifranky98@gmail.com>
2025-02-04 09:02:52 +0100

    filtri ok

--- products-page/script.js
+++ products-page/script.js
@@ -1,61 +1,26 @@
-function fetchAllProducts() {
+let allProducts = [];
+function fetchAllProducts()
+{
   fetch("https://fakestoreapi.com/products")
     .then((response) => response.json())
-    .then((products) => {
+    .then((products) =>
+    {
       console.log(products);
+      allProducts = products; // Per utilizzarlo nella funzione categoryFilter
       displayProducts(products);
     })
-    .catch((error) => console.error("Errore nel recuper dei prodotti", error));
+    .catch((error) => console.error("Errore nel recupero dei prodotti", error));
 }
 
-function fetchProductsByCategory(category) {
-  fetch("https://fakestoreapi.com/products/category/" + category)
-    .then((response) => response.json())
-    .then((products) => {
-      console.log(products);
-      displayProducts(products);
-    })
-    .catch((error) => console.error("Errore nel recuper dei prodotti", error));
-}
-
-let selectedCategories = [];
-
-function fetchProductsByCategories(categories) {
-  const promises = categories.map((category) =>
-    fetch("https://fakestoreapi.com/products/category/" + category).then(
-      (response) => response.json()
-    )
-  );
-
-  Promise.all(promises)
-    .then((results) => {
-      const allProducts = results.flat();
-      displayProducts(allProducts);
-    })
-    .catch((error) => console.error("Errore nel recuper dei prodotti", error));
-}
-
-function filterCheck(element, category) {
-  if (element.checked) {
-    selectedCategories.push(category);
-  } else {
-    selectedCategories = selectedCategories.filter((cat) => cat !== category);
-  }
-
-  if (selectedCategories.length > 0) {
-    fetchProductsByCategories(selectedCategories);
-  } else {
-    fetchAllProducts();
-  }
-}
-
-function displayProducts(products) {
+function displayProducts(products)
+{
   const product = products
-    .map((product) => {
+    .map((product) =>
+    {
       return `
-            <div class="col-md-4 mb-4">
+            <div class="col-md-4 mb-4" id="${product.id}">
             <div class="card">
-                <img src="${product.image}" class="card-img-top" alt="...">
+                <img src="${product.image}" class="card-img-top" alt="${product.title}">
                 <div class="card-body">
                 <h5 class="card-title">${product.title}</h5>
                 <p class="card-text">${product.description}</p>
@@ -72,6 +37,29 @@ function displayProducts(products) {
   ).innerHTML = `<div class="row">${product}</div>`;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+function categoryFilter() 
+{
+  const checkedCheckboxes = Array.from(document.querySelectorAll('input[id="flexCheckChecked"]:checked')); // Mette in un array le checkbox selezionate
+  const selectedCategories = checkedCheckboxes.map(checkbox => checkbox.value); // Ottiene le categorie selezionate dai valori assegnati alle checkbox
+  if (selectedCategories.length === 0) 
+  {
+    allProducts;
+    return;
+  }
+  // Per ogni prodotto, se nelle categorie selezionate c'è la categoria del prodotto, rimuove il display none, altrimenti lo aggiunge
+  allProducts.forEach(product =>
+  {
+    if (selectedCategories.includes(product.category))
+    {
+      document.getElementById(product.id).classList.remove('d-none');
+    } else
+    {
+      document.getElementById(product.id).classList.add('d-none');
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () =>
+{
   fetchAllProducts();
 });
